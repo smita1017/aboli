fdc1682b70fd002ad365b2186caf18b489d69a85
smita1017 <smita126@outlook.com>
2024-10-07 06:52:00 +0000

    header page implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*"],
   theme: {
+    colors: {
+      'primary': '#D9A98D',
+      'secondary': '#EED9BE',
+      'ternery': '#ff7849',
+      'dark': '#222121',
+      'white': '#FFFFFF',
+    },
     extend: {},
+    fontFamily: {
+      primary: ['Condiment', 'cursive'],
+      serif: ['Nunito', 'serif'],
+    },
   },
   plugins: [],
 }
